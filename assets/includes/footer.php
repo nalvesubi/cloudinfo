4777d718e7bd74ae011857a179211fd5d16b3e40
nalvesubi <35038154+nalvesubi@users.noreply.github.com>
2018-08-29 15:37:31 +0100

    Create footer.php

---
+++ assets/includes/footer.php
@@ -0,0 +1,40 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-7 order-sm-1 col-12 order-12">
+				<div class="d-none d-sm-block">
+					<nav class="nav">
+						<a class="nav-link" href="#">Company</a>
+						<a class="nav-link" href="#">Contact Us</a>
+						<a class="nav-link" href="#">Shipping</a>
+						<a class="nav-link" href="#">Returns</a>
+						<a class="nav-link" href="#">Careers</a>
+					</nav>
+				</div>
+				<nav class="footer-social">
+					<a class="social-link" href="#"><i class="icon-facebook-with-circle"></i></a>
+					<a class="social-link" href="#"><i class="icon-twitter-with-circle"></i></a>
+					<a class="social-link" href="#"><i class="icon-pinterest-with-circle"></i></a>
+					<a class="social-link" href="#"><i class="icon-tumblr-with-circle"></i></a>
+					<a class="social-link" href="#"><i class="icon-google-with-circle"></i></a>
+				</nav>
+				<p class="copyright">&copy 2015 Merlin's Potions. All Rights Reserved</p>
+			</div>
+			<div class="col-sm-5 order-sm-7 col-12 order-1">
+				<div class="card">
+					<div class="card-body">
+						<h3>Sign up for our Newsletter</h3>
+						<p>Sign up for our newsletter & get exclusive discount</p>
+						<form class="">
+							  <div class="input-group">
+								<label for="newsletter" class="sr-only">Sign up for our newsletter & get exclusive discount</label>
+								<input type="email" class="form-control col-8" id="newsletter" placeholder="Enter your email">
+								<button type="submit" class="btn btn-newsletter col-4">Submit</button>
+							  </div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
