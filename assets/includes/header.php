055da49a41db808f0a75421325f4dd893fa7f4c9
nalvesubi <35038154+nalvesubi@users.noreply.github.com>
2018-08-29 15:37:14 +0100

    Create header.php

---
+++ assets/includes/header.php
@@ -0,0 +1,140 @@
+<header>
+	<div class="container">
+		<div class="row">
+			<div class="col-2 d-block d-sm-none">
+			<!-- toggle menu mobile -->
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-merlin" aria-controls="navbar-merlin" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			<!-- toggle menu mobile -->
+			</div>
+			<div class="col-sm-4 col-md-3 col-7">
+			<!-- logo -->
+				<div class="header-logo">
+					<a href="#" class="header-link">MERLIN'S <span>POTIONS</span><div>fine potions since 1026</div></a>
+				</div>
+			<!-- logo -->
+			</div>
+			<div class="col-sm-6 d-none d-sm-block">
+			<!-- search bar -->
+				<div class="row">
+					<div class="col-8 d-none d-md-block">
+						<form class="header-search">
+							<div class="input-group col-10">
+								<div class="input-group-prepend">
+									<span class="input-group-text">
+										<i class="icon-search" aria-hidden="true"></i>
+									</span>
+								</div>
+								<label for="search-products" class="sr-only">Search our stock</label>
+								<input type="text" class="form-control" id="search-products" aria-describedby="search-products" placeholder="Search our stock">
+							</div>
+						</form>
+					</div>
+					<div class="col-4 header-call">
+						<p>Free shipping <span>on orders over $50</span></p>
+					</div>
+				</div>
+			<!-- search bar -->
+			</div>
+			<div class="col-sm-2 col-3">
+			<!-- cart -->
+				<div class="header-cart">
+				<img src="assets/header-cart-icon.png" width="50" height="50" class="header-cart-icon" alt="Bag" title="Bag">
+				Bag: <span id="cart-qty"></span>
+				</div>
+			</div>
+		</div>
+		<div class="row d-block d-sm-none">
+			<div class="col-12 header-call-mobile">
+				<p>Free shipping <span>on orders over $50</span></p>
+			</div>
+		</div>
+	</div>
+	<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+		<div class="container">
+			<div class="collapse navbar-collapse" id="navbar-merlin">
+				<div class="d-block d-sm-none header-mobile-search">
+					<div class="row">
+						<div class="col-2">
+							<button class="btn btn-link navbar-mobile-toggler" type="button" data-toggle="collapse" data-target="#navbar-merlin" aria-controls="navbar-merlin" aria-expanded="false" aria-label="Toggle navigation">
+								X
+							</button>
+						</div>
+						<div class="col-10">
+							<form class="header-search">
+								<div class="input-group col-10">
+									<div class="input-group-prepend">
+										<span class="input-group-text">
+											<i class="icon-search" aria-hidden="true"></i>
+										</span>
+									</div>
+									<label for="search-products" class="sr-only">Search our stock</label>
+									<input type="text" class="form-control" id="search-products" aria-describedby="search-products" placeholder="Search our stock">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Potions
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">#</a>
+							<a class="dropdown-item" href="#">#</a>
+							<a class="dropdown-item" href="#">#</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Ingredients
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">#</a>
+							<a class="dropdown-item" href="#">#</a>
+							<a class="dropdown-item" href="#">#</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Books
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">#</a>
+							<a class="dropdown-item" href="#">#</a>
+							<a class="dropdown-item" href="#">#</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Supplies
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">#</a>
+							<a class="dropdown-item" href="#">#</a>
+							<a class="dropdown-item" href="#">#</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Charms
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">#</a>
+							<a class="dropdown-item" href="#">#</a>
+							<a class="dropdown-item" href="#">#</a>
+						</div>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link btn btn-clearance" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Clearance!
+						</a>
+					</li>
+				</ul>
+				<span class="navbar-text"><a href="#">Sign In</a> or <a href="#">Sign Up</a></span>
+			</div>
+		</div>
+	</nav>
+</header>
